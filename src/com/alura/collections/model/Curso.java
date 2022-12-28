e8536f45a589f4234ca5abf0a8d0188014f0b8de
jocnn <ijann_@outlook.com>
2022-12-28 19:08:21 -0300

    sets iterators - otros tipos

--- src/com/alura/collections/model/Curso.java
+++ src/com/alura/collections/model/Curso.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+//import java.util.LinkedHashSet;
 import java.util.List;
 
 public class Curso{
@@ -11,6 +12,7 @@ public class Curso{
 	private int tiempo;
 	private List<Aula> aulaList = new ArrayList<>();
 	private Collection<Alumno> alumnos = new HashSet<>();
+	//private Collection<Alumno> alumnos = new LinkedHashSet<>();
 	
 	public Curso(String nombre, int tiempo) {
 		this.nombre = nombre;
